efe304afc23466940c3f8ad17e0048f5534a67dd
BoppleOpple <boppleopple@gmail.com>
2025-03-16 13:53:37 -0400

    registerCommands fixed, no longer used as a module

--- src/registerCommands.ts
+++ src/registerCommands.ts
@@ -20,7 +20,7 @@ declare type command = {
  * This function handles all of the required api calls to register the bot's commands.
  * @param commands An array of commands to be registered
  */
-export async function updateCommands(commands: command[]): Promise<void> {
+async function updateCommands(commands: command[]): Promise<void> {
     // discord.js handles api endpoints for us with the REST object
     const rest = new REST({ version: "10" }).setToken(
         process.env.DISCORD_TOKEN as string,
@@ -41,8 +41,9 @@ export async function updateCommands(commands: command[]): Promise<void> {
     }
 }
 
-export async function main(): Promise<void> {
-    await exports.updateCommands([
+async function main(): Promise<void> {
+    console.log("Registering commands...");
+    await updateCommands([
         {
         name: "ping",
         description: 'test bot and return "pong"',
@@ -80,3 +81,5 @@ export async function main(): Promise<void> {
         }
     ]);
 }
+
+main();
